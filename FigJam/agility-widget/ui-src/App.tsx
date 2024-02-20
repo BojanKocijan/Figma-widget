2e0eff5a65ddb5eff88870c87796e393b094d365
kocijan <23736763+BojanKocijan@users.noreply.github.com>
2024-02-20 11:50:46 +0100

    Code on repo
    This is my first repo for wigJam widget

---
+++ FigJam/agility-widget/ui-src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    if (typeof parent !== undefined) {
+      parent?.postMessage?.({ pluginMessage: "Agility work item imported" }, "*");
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>Agility widget test fkjlsdgkg 3333A</h3>
+      <p>This is just proof that we can add a widget and work on it and make something cool</p>
+      <button
+        onClick={() => {
+          parent?.postMessage?.({ pluginMessage: "close" }, "*");
+        }}
+      >
+        Close
+      </button>
+    </div>
+  );
+}
+
+export default App;
